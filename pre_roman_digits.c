731697695c6c31742e5e1227d631d20fda8a43a4
vinaykv <vinaykva@gmail.com>
2016-09-29 07:51:45 -0700

    resolved error with pointers in pre_digit_roman helper function

--- pre_roman_digits.c
+++ pre_roman_digits.c
@@ -6,8 +6,8 @@ char* pre_roman_digit_characters(char character1,char character2,char* buffer)
 {
 /*this function stores the previous digits of the roman character in temproary memory buffer*/
 	*buffer = character1;
-	*(buffer++);
+	buffer++;
 	*buffer = character2;
-	*(buffer++);
+	buffer++;
 	return buffer;
 }
