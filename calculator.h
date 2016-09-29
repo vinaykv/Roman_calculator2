1cc2dc7e0adf079e2b2a8456a08532727b656087
vinaykv <vinaykva@gmail.com>
2016-09-29 12:04:21 -0700

    still facing error in roman addition

--- calculator.h
+++ calculator.h
@@ -1,6 +1,6 @@
 #ifndef calculator_H
 #define calculator_H
 
-char* calculate_roman_numbers(char first_roman_number[CHAR_LENGTH],char second_roman_number[CHAR_LENGTH], char Add_Subtract[ADD_SUBTRACT],char* buffer);
+char* calculate_roman_numbers(char first_roman_number[],char second_roman_number[], char Add_Subtract[],char* buffer);
 
 #endif
