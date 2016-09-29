96c8c1c586e0de0363a38c45ad1e6b228eb02c62
vinaykv <vinaykva@gmail.com>
2016-09-29 07:15:25 -0700

    return the value of M without warning

--- post_roman_digits.c
+++ post_roman_digits.c
@@ -3,13 +3,13 @@
 #include "decimal_value_to_roman_string.h"
 
 //storing the post digits
-char* post_roman_digit_characters(const char character,const int count,char* buffer)
+char* post_roman_digit_characters(char character,int count,char* buffer)
 {
 	/*this function stores the post digits of the roman character in temproary memory buffer*/
 	static int character_count;
 	for(character_count = 0;character_count < count;character_count++){
 		*buffer = character;
+		buffer++;
 	}
-	buffer++;
 	return buffer;
 }
