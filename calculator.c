b87f68975695ad6974849152cb3ec0e24bc60787
vinaykv <vinaykva@gmail.com>
2016-09-29 12:33:14 -0700

    error in addtion of two numbers

--- calculator.c
+++ calculator.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include "roman_string_to_decimal_value.h"
 #include "decimal_value_to_roman_string.h"
 #include "calculator_addition.h"
 #include "calculator_subtraction.h"
-# define CHAR_LENGTH 50
-# define ADD_SUBTRACT 10
+#define LIMIT 50
 
-char* calculate_roman_numbers(char first_roman_number[CHAR_LENGTH],char second_roman_number[CHAR_LENGTH], char Add_Subtract[ADD_SUBTRACT],char* buffer)
+char* calculate_roman_numbers(char first_roman_number[],char second_roman_number[], char Add_Subtract[])
 
 	{
 	/*calculate function reveives the input from test file first_roman number,
  		second roman_number, addition/subtraction operation and temproary memory */
 
+	char buffer[LIMIT];
 	int first_number = roman_string_to_decimal_value(first_roman_number); // calling roman_decimal function to convert first roman number to decimal
 	int second_number = roman_string_to_decimal_value(second_roman_number); // calling roman_decimal function to convert second roman number to decimal
 	char * final_answer; // to store the final result
 	
 	if((strcmp(Add_Subtract,"ADD") == 0)|| (strcmp(Add_Subtract,"add") == 0)) // check for the addition operation
 	{
-		final_answer = addition_of_roman_numbers(first_number,second_number);		
+		final_answer = addition_of_roman_numbers(first_number,second_number,buffer);		
 	}
 
 return final_answer;
--- calculator_addition.h
+++ calculator_addition.h
@@ -1,6 +1,6 @@
 #ifndef calculator_addition_H
 #define calculator_addition_H
 
-char* addition_of_roman_numbers(int first_number,int second_number);
+char* addition_of_roman_numbers(int first_number,int second_number,char* buffer);
 
 #endif
