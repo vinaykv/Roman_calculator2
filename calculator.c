beb79932386799d218d75f28e7b9ed32f464f018
vinaykv <vinaykva@gmail.com>
2016-09-29 23:25:44 -0700

    removed unwanted variables from calculator.c file

--- calculator.c
+++ calculator.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include "roman_string_to_decimal_value.h"
 #include "decimal_value_to_roman_string.h"
-#include "calculator_addition.h"
 #define FAILURE -1
 #define INVALID "-1"
 #define MAX_VALUE 3999
@@ -44,33 +43,30 @@ char* calculate_roman_numbers(char first_roman_number[],char second_roman_number
 
 char* addition_of_roman_numbers(int first_sum_number,int second_sum_number,char* buffer)
 {
-	char* sum_of_roman_value;
+	
 	if((first_sum_number + second_sum_number) > MAX_VALUE)
 		{
 			return INVALID;
 		}
-		else{
+		else
 			/*calculate function calls decimal_number_to_roman_string function to convert decimal to roman value*/
-			  sum_of_roman_value = decimal_number_to_roman_string((first_sum_number + second_sum_number),buffer);
-	} /* END of addition operation*/
-return sum_of_roman_value;
+			  return decimal_number_to_roman_string((first_sum_number + second_sum_number),buffer);
+	 /* END of addition operation*/
+
 }
 
 char* subtraction_of_roman_numbers(int first_roman_integer,int second_roman_integer,char* sub_buffer)
 {
-	char* subtraction_of_roman_value;
+	
 	if((first_roman_integer - second_roman_integer) > MAX_VALUE || (first_roman_integer - second_roman_integer) < MIN_VALUE)
 		{
 			return INVALID;
 		}
-		else{
+		else
 			/*calculate function calls decimal_number_to_roman_string function to convert decimal to roman value*/
-			subtraction_of_roman_value = decimal_number_to_roman_string(abs(first_roman_integer - second_roman_integer),sub_buffer);
+			return decimal_number_to_roman_string(abs(first_roman_integer - second_roman_integer),sub_buffer);
 		/*Note: As there are no negative numbers in roman
 		  considering the absolute value of the negative number*/
-}
-return subtraction_of_roman_value;
-
 }
 
 
