fa82016ddaef9d978beacd658a49144ce28bf3ac
vinaykv <vinaykva@gmail.com>
2016-09-29 23:15:59 -0700

    refactoring done for calculator file added helper function addition and subtraction

--- calculator.h
+++ calculator.h
@@ -2,6 +2,7 @@
 #define calculator_H
 
 char* calculate_roman_numbers(char first_roman_number[],char second_roman_number[],char Add_Subtract[],char* buffer);
-
+char* addition_of_roman_numbers(int first_sum_number,int second_sum_number,char* buffer);
+char* subtraction_of_roman_numbers(int first_roman_integer,int second_roman_integer,char* sub_buffer);
 
 #endif
